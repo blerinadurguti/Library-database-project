a947998fdd519948feddbe7a5443c96d5394c55c
blerinadurguti <93934589+blerinadurguti@users.noreply.github.com>
2022-01-03 14:27:31 +0200

    Update storedprocedures.sql

--- storedprocedures.sql
+++ storedprocedures.sql
@@ -1,3 +1,5 @@
+-- Shembull 1
+
 -- Procedure qe i gjen lexueset femra qe jane nxenese
 
 delimiter $$
@@ -9,3 +11,22 @@ where readerGender=gender and readerOccupation=occupation;
 end $$
 
 call nxeneseFemra('F', 'Nxenese');
+
+
+-- Shembull 2
+
+-- Procedure qe tregon se libri eshte i ri dhe
+-- eshte huazur me 12 tetor
+
+delimiter $$
+create procedure libri_iri()
+begin
+
+select distinct borrowedBookID, borrowDate, bookTitle, bookCondition
+from Huazimi, Libri
+where huazimi.borrowedBookID=libri.bookID
+and libri.bookCondition='E re' and huazimi.borrowDate='2021-10-12';
+end $$
+delimiter $$
+
+call libri_iri();
